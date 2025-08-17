90b28ee153dea8b177ee45620376c64da465cc63
rustpranker <skirezik1024@gmail.com>
2025-08-17 10:33:57 +0300

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBi9hhKmd1M-lgWQiweCedaENKAu5ATJvg",
+  authDomain: "test-f5bde.firebaseapp.com",
+  projectId: "test-f5bde",
+  storageBucket: "test-f5bde.appspot.com",
+  messagingSenderId: "803807833739",
+  appId: "1:803807833739:web:b259deb2fb2fead9d2ee8e",
+  measurementId: "G-JXNNEMMRDC"
+};
